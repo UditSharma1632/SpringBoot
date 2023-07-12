e087fed70bf992c2da948677a626330482496cbe
Udit Sharma <uditsharma1632@gmail.com>
2023-07-12 15:41:20 +0530

    Implemented Mapper for DTO using ModelMapper pattern

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SpringBootREST
+Feature-4: We will be using **_model mapper or map struct_** to convert entity into DTO
--- src/main/java/com/learning/springbootrest/service/StudentServiceImpl.java
+++ src/main/java/com/learning/springbootrest/service/StudentServiceImpl.java
@@ -2,11 +2,10 @@ package com.learning.springbootrest.service;
 
 import com.learning.springbootrest.dto.StudentDto;
 import com.learning.springbootrest.entity.Student;
-import com.learning.springbootrest.mapper.StudentMapper;
 import com.learning.springbootrest.repository.StudentRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,14 +16,16 @@ public class StudentServiceImpl implements StudentService{
 
     StudentRepository studentRepo;
 
+    ModelMapper modelMapper;
+
     @Override
     public StudentDto createStudent(StudentDto studentDto) {
 
-        Student student = StudentMapper.mapToStudent(studentDto);
+        Student student = modelMapper.map(studentDto, Student.class);
 
         Student savedStudent = studentRepo.save(student);
 
-        StudentDto updatedStudentDto = StudentMapper.mapToStudentDto(savedStudent);
+        StudentDto updatedStudentDto = modelMapper.map(savedStudent, StudentDto.class);
 
         return updatedStudentDto;
     }
@@ -33,26 +34,26 @@ public class StudentServiceImpl implements StudentService{
     public StudentDto getStudentById(Long id) {
         Optional<Student> optionalStudent= studentRepo.findById(id);
         Student student = optionalStudent.get();
-        return StudentMapper.mapToStudentDto(student);
+        return modelMapper.map(student, StudentDto.class);
     }
 
     @Override
     public List<StudentDto> getAll() {
         List<Student> students = studentRepo.findAll();
-        return students.stream().map(StudentMapper::mapToStudentDto).collect(Collectors.toList());
+        return students.stream().map((student -> modelMapper.map(student, StudentDto.class)))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public StudentDto updateStudent(StudentDto studentDto) {
-        Student student = StudentMapper.mapToStudent(studentDto);
+    public StudentDto updateStudent(StudentDto student) {
         Student existingStudent = studentRepo.findById(student.getId()).get();
         existingStudent.setFirstName(student.getFirstName());
         existingStudent.setLastName(student.getLastName());
         existingStudent.setEmail(student.getEmail());
         existingStudent.setBranch(student.getBranch());
         Student updatedStudent = studentRepo.save(existingStudent);
-        StudentDto updatedStudentDto = StudentMapper.mapToStudentDto(updatedStudent);
-        return updatedStudentDto;
+        return modelMapper.map(updatedStudent, StudentDto.class);
+
     }
 
     @Override
