3f29ce6f3749c033ea4e1ee8f0bd79d73535699b
Udit Sharma <uditsharma1632@gmail.com>
2023-07-14 15:09:01 +0530

    Added swagger documentation and completed basic CRUD learning

--- src/main/java/com/learning/springbootrest/SpringBootRestApplication.java
+++ src/main/java/com/learning/springbootrest/SpringBootRestApplication.java
@@ -1,11 +1,26 @@
 package com.learning.springbootrest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "CRUD simple api",
+                description = "This is simple CRUD project",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Udit Sharma",
+                        email = "uditsharma1632@gmail.com",
+                        url = "https://github.com/UditSharma1632"
+                )
+        )
+)
 public class SpringBootRestApplication {
     @Bean
     public ModelMapper modelMapper(){
--- src/main/java/com/learning/springbootrest/controller/StudentController.java
+++ src/main/java/com/learning/springbootrest/controller/StudentController.java
@@ -2,6 +2,9 @@ package com.learning.springbootrest.controller;
 
 import com.learning.springbootrest.dto.StudentDto;
 import com.learning.springbootrest.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,7 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
+@Tag(
+        name = "CRUD rest api for Student",
+        description = " We will perform create get update and delete operations on student object"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("students")
@@ -18,35 +24,79 @@ public class StudentController {
     StudentService studentService;
 
     // post request using dto object
+    @Operation(
+            summary = "This will create new Student"
+    )
+    @ApiResponse(
+            responseCode = "201"
+    )
     @PostMapping("create")
     public ResponseEntity<StudentDto> createStudent(@RequestBody @Valid StudentDto studentDto){
         StudentDto student = studentService.createStudent(studentDto);
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
-    // get method using dto object
 
+
+
+    // get method using dto object
+    @Operation(
+            summary = "This will fetch Student by id"
+    )
+    @ApiResponse(
+            responseCode = "200"
+    )
     @GetMapping("get/{id}")
     public ResponseEntity<StudentDto> getStudent(@PathVariable("id") Long id){
        StudentDto studentDto = studentService.getStudentById(id);
         return ResponseEntity.ok(studentDto);
     }
 
+
+
+
+
+
     // get method using dto object
+    @Operation(
+            summary = "This will fetch Student by id and we will use query param"
+    )
+    @ApiResponse(
+            responseCode = "200"
+    )
     @GetMapping("get")
     public ResponseEntity<StudentDto> getStudentByParam(@RequestParam Long id){
         StudentDto studentDto = studentService.getStudentById(id);
         return ResponseEntity.ok(studentDto);
     }
 
-    // get method to obtain every user from db
+
+
+
+
+    // get method to obtain every student from db
+    @Operation(
+            summary = "This will fetch all Students"
+    )
+    @ApiResponse(
+            responseCode = "200"
+    )
     @GetMapping("getAll")
     public ResponseEntity<List<StudentDto>> getStudents(){
         List<StudentDto> studentDto = studentService.getAll();
         return ResponseEntity.ok(studentDto);
     }
 
+
+
+
     // update method to update student details
+    @Operation(
+            summary = "This will update a single Student by using its id"
+    )
+    @ApiResponse(
+            responseCode = "200"
+    )
     @PutMapping("update/{id}")
     public ResponseEntity<StudentDto> getStudent(@PathVariable Long id, @RequestBody @Valid StudentDto studentDto){
         studentDto.setId(id);
@@ -54,7 +104,16 @@ public class StudentController {
         return ResponseEntity.ok(updatedStudent);
     }
 
+
+
+
     // delete api
+    @Operation(
+            summary = "This will delete a single Student by using its id"
+    )
+    @ApiResponse(
+            responseCode = "200"
+    )
     @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deleteStudent(@PathVariable("id") Long id){
         studentService.deleteStudent(id);
